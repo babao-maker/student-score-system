f534d96f7996fe7a1ff65081d2f9a5bbf15db0fe
wqhhh619 <3384068351@qq.com>
2026-09-20 19:21:19 +0800

    Add student name input feature

--- student.py
+++ student.py
@@ -1,7 +1,12 @@
-def show_student():
+def add_name(student_dict):
     name = input("请输入学生姓名：")
-    print(f"学生姓名：{name}")
-
+    if name in student_dict:
+        print(f"学生【{name}】已存在！")
+        return
+    # 只新增姓名，成绩预留为空，后续录入成绩功能填充
+    student_dict[name] = ""
+    print(f"学生【{name}】姓名录入成功，请后续录入成绩！")
 
 if __name__ == "__main__":
-    show_student()
+    student_dict = {}
+    add_name(student_dict)
