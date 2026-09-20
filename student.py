cb1625939575ee630d72e30ee035b240b187a653
qiqi0108 <2472119976@qq.com>
2026-09-20 16:54:55 +0800

    Add student score input feature

--- student.py
+++ student.py
@@ -1,6 +1,23 @@
-def show_student():
-    print("Student Management System")
+def add_score(student_dict):
+    # 录入学生姓名
+    name = input("请输入学生姓名：")
+    # 判断学生是否已经存在（已经提前录入姓名）
+    if name not in student_dict:
+        print(f"错误：不存在学生【{name}】，请先录入该学生姓名！")
+        return
+
+    # 录入成绩，转成数字
+    try:
+        score = float(input("请输入学生成绩："))
+    except ValueError:
+        print("成绩输入错误，请输入数字！")
+        return
+    # 更新成绩
+    student_dict[name] = score
+    print(f"学生【{name}】成绩【{score}】录入成功！")
 
 
 if __name__ == "__main__":
-    show_student()
+    student_dict = {}
+    # 再录入成绩
+    add_score(student_dict)
