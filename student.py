7b592930972627d940f58daffe2ce3944bad0a27
babao-maker <2138163264@qq.com>
2026-09-21 09:11:10 +0800

    新增modify_score函数，实现修改学生成绩功能

--- student.py
+++ student.py
@@ -40,7 +40,19 @@ def show_student(student_dict):
     else:
         print("未找到该学生信息！")
 
-   
+# 修改学生成绩
+def modify_score(student_dict):
+    name = input("请输入要修改成绩的学生姓名：")
+    if name not in student_dict:
+        print("未找到该学生！")
+        return
+    try:
+        new_score = float(input("请输入新成绩："))
+    except ValueError:
+        print("成绩输入错误，请输入数字！")
+        return
+    student_dict[name] = new_score
+    print(f"学生【{name}】成绩修改成功，新成绩：{new_score}")   
 if __name__ == "__main__":
     student_dict = {}
     # 再录入成绩
@@ -48,3 +60,4 @@ if __name__ == "__main__":
 
     add_score(student_dict)
     show_student(student_dict)
+    modify_score(student_dict)
